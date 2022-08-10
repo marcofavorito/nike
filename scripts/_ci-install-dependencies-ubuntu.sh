ea76808982724aed6287f226ba5f34206450a515
MarcoFavorito <marco.favorito@gmail.com>
2022-08-10 18:24:42 +0200

    ci: add CUDD as dependency

--- scripts/_ci-install-dependencies-ubuntu.sh
+++ scripts/_ci-install-dependencies-ubuntu.sh
@@ -1,3 +1,9 @@
 #!/usr/bin/env bash
 
 set -e
+
+wget https://github.com/whitemech/cudd/releases/download/v3.0.0/cudd_3.0.0_linux-amd64.tar.gz
+tar -xf cudd_3.0.0_linux-amd64.tar.gz
+cd cudd_3.0.0_linux-amd64
+sudo cp -P lib/* /usr/local/lib/
+sudo cp -Pr include/* /usr/local/include
