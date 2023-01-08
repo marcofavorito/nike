e57e9bf37783fba1ad7db4570f3cd5c5c1838b71
MarcoFavorito <marco.favorito@gmail.com>
2023-01-08 21:15:57 +0100

    remove bdd mode in multithreaded mode

--- libs/core/src/multithreaded.cpp
+++ libs/core/src/multithreaded.cpp
@@ -57,8 +57,7 @@ void ThreadedForwardSynthesis::stop() { syn->stop(); }
 void ThreadedForwardSynthesis::join() { t.join(); }
 
 bool MultithreadedSynthesis::is_realizable() {
-  std::vector<StateEquivalenceMode> modes{StateEquivalenceMode::HASH,
-                                          StateEquivalenceMode::BDD};
+  std::vector<StateEquivalenceMode> modes{StateEquivalenceMode::HASH};
 
   std::vector<BranchingStrategy> bs{
       BranchingStrategy::TRUE_FIRST,
